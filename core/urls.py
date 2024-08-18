d5c96aaf25d5af88c9a6261c88325f6b9f017fd3
seckrel <aayam.oza@gmail.com>
2024-08-18 23:20:37 +0545

    feat: api to download transaction as pdf

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from core.viewsets.transaction_viewsets import TransactionModelViewSet
+from core.views import ListTransactionPdf
 
 router = DefaultRouter()
 
 router.register('transactions', TransactionModelViewSet)
 
 urlpatterns = [
-    path("transactions/pdf/", )
+    path("pdf/transactions/", ListTransactionPdf.as_view())
 ]
 
 urlpatterns += router.urls
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.template.loader import get_template
+from core.models import Transaction
+from xhtml2pdf.pisa import CreatePDF
+from django.http import HttpResponse
 
 
 class ListTransactionPdf(APIView):
@@ -9,4 +13,20 @@ class ListTransactionPdf(APIView):
     Args:
         APIView (file): PDF with transaction detail
     """
-    pass
+    allowed_methods = ['get']
+
+    def get(self, request):
+        template = get_template('core/pdf/all_transaction_list.html')
+        transaction_list = Transaction.objects.all()
+        pdf_html = template.render({
+            'transactions': transaction_list
+        })
+
+        response = HttpResponse(content_type='application/pdf')
+        pdf_status = CreatePDF(pdf_html, dest=response)
+
+        if pdf_status.err:
+            return Response('Error in generating PDF. Try again', status=400)
+        
+        response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+        return response
