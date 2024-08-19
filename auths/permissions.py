759ae0a514bacfc66d32edf7879957a92c508c99
seckrel <aayam.oza@gmail.com>
2024-08-19 18:25:19 +0545

    fix: reject all anonymous user request

--- auths/permissions.py
+++ auths/permissions.py
@@ -3,9 +3,14 @@ from rest_framework.permissions import BasePermission, IsAuthenticated, SAFE_MET
 
 class IsManagerPermission(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
+
         return request.user.is_manager
 
 
 class IsStaffPermission(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
         return request.user.is_staff
--- core/permissions.py
+++ core/permissions.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class StaffAllowedPermission(BasePermission):
     def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
+
         staff_allowed_http = ['POST', 'GET', 'PUT', 'PATCH']
         if request.user.is_staff and 'transaction_status' in request.data:
             return False
